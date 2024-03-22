06cb7f65db93a6685309c8d0d01a8cb3ff6ba54b
K1ngHandy <bolds-slicks0g@icloud.com>
2024-03-22 01:19:44 -0400

    for loops

---
+++ demo.js
@@ -0,0 +1,21 @@
+let a = [ 1, 2, 3, 4 ];
+
+// for (let i = 0; i < a.length; i++) { // loop through array length
+//     const current = a[i];
+//     console.log(current);
+// }
+
+// for (let i = 0; i < a.length - 1; i++) { // loop through first 3 indices
+//     const current = a[i];
+//     const next = a[i] + 1;
+//     console.log(current);
+//     console.log(next);
+// }
+
+// for (let x of a) { // for of loop
+//     console.log(x);
+// }
+
+for (let x in a) {
+    
+}
