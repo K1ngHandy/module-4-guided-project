dbb24beaf50c0276132de0a28db697f3f4c8e916
K1ngHandy <bolds-slicks0g@icloud.com>
2024-03-25 12:47:43 -0400

    string methods

--- demo.js
+++ demo.js
@@ -38,12 +38,48 @@ let a = [ 1, 2, 3, 4 ];
 // }
 // console.log(start);
 
-let menuOption;
+// let menuOption;
 
-do { // do while loop - run logic at least once and possibly again after
-    console.log('pick 1 to run, or 0 to quit');
-    menuOption = 0;
-    if (menuOption !== 0) {
-        // something
-    }
-} while (menuOption === 1);
+// do { // do while loop - run logic at least once and possibly again after
+//     console.log('pick 1 to run, or 0 to quit');
+//     menuOption = 0;
+//     if (menuOption !== 0) {
+//         // something
+//     }
+// } while (menuOption === 1);
+
+// let myString = 'hello world';
+// for (let i = 0; i < myString.length; i++) { // loop through characters in string
+//     const charAtIndex = myString[i];
+//     console.log(charAtIndex);
+// }
+
+// let str = 'my string';
+// if (typeof str === 'string') console.log('its a string');
+
+// console.log(str);
+// str[1] = 'l'; // will not change string because strings are immutable. will need to reassign string
+// console.log(str);
+// console.log(str.length);
+
+// let str = 'my string';
+// // slice()
+// let str1 = str.slice(1);
+// let str2 = str.slice(1, 4);
+// let str3 = str.slice(-3, -1);
+// // substring()
+// let str4 = str.substring(1);
+// let str5 = str.substring(1, 4);
+// // substr()
+// let str6 = str.substr(1);
+// let str7 = str.substr(2, 2);
+// console.log(`str1: ${str1}\nstr2: ${str2}\nstr3: ${str3}\nstr4: ${str4}\nstr5: ${str5}\nstr6: ${str6}\nstr7: ${str7}\n`);
+
+let str = 'This is a string as an example for this';
+let str1 = str.toUpperCase(); // toUppercase
+let str2 = str.toLowerCase(); // toLowercase
+let str3 = str.replace('this', 'hello'); // replace 1st instance of 'this'
+let str4 = str.replaceAll('this', 'hello'); // replace all instances of 'this'
+let index1 = str2.indexOf('this');
+let index2 = str2.lastIndexOf('this');
+console.log(`str1: ${str1}\nstr2: ${str2}\nstr3: ${str3}\nstr4: ${str4}\nindex1: ${index1}\nindex2: ${index2}\n`);
